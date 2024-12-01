29b97cd00d90c376e5b063e72c8d35aef1cf9475
pablitoodt <pablodeteba@icloud.com>
2024-12-01 11:57:08 +0100

    day_1: part_2

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -27,3 +27,32 @@ def day1_part1():
     return total
 
 print(day1_part1())
+
+def day1_part2():
+    data = open("day_1/input.txt").read().strip()
+
+    tab1 = []
+    tab2 = []
+    total = 0
+    times = 0
+
+    for line in data.split('\n'):
+        index = 0
+        for data in line.split('   '):
+            if (index == 0):
+                tab1.append(int(data))
+                index += 1
+            elif (index == 1):
+                tab2.append(int(data))
+                index -=1
+    
+    for number in tab1:
+        for equal in tab2:
+            if (number == equal):
+                times += 1
+        total += (number * times)
+        times = 0
+    
+    return total
+
+print(day1_part2())
